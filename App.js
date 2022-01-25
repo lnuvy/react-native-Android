a1876bcdfbbb4733bc13001d46d22962e3959dcf
lnuvy <hangeulgod@naver.com>
2022-01-25 16:36:28 +0900

    Theme 으로 간단하게 다크모드 적용

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Text, Image } from "react-native";
+import { Text, Image, useColorScheme } from "react-native";
 import * as Font from "expo-font";
 import { useFonts } from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+} from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 
 export default function App() {
@@ -13,12 +17,14 @@ export default function App() {
   const [assets] = useAssets([require("./picture.jpg")]);
   const [loaded] = useFonts(Ionicons.font);
 
+  const isDark = useColorScheme() === "dark";
+
   if (!assets || !loaded) {
     return <AppLoading />;
   }
   return (
     // 사용하려면 반드시 임포트 필요
-    <NavigationContainer>
+    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
       <Tabs />
     </NavigationContainer>
   );
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -10,23 +10,9 @@ import { BLACK_COLOR, GRAY_COLOR, YELLOW_COLOR } from "../colors";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-  const isDark = useColorScheme() !== "dark";
+  const isDark = useColorScheme() === "dark";
   return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarStyle: {
-          backgroundColor: isDark ? BLACK_COLOR : "white",
-        },
-        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
-        tabBarInactiveTintColor: GRAY_COLOR,
-        headerStyle: {
-          backgroundColor: isDark ? BLACK_COLOR : "white",
-        },
-        headerTitleStyle: {
-          color: isDark ? YELLOW_COLOR : BLACK_COLOR,
-        },
-      }}
-    >
+    <Tab.Navigator>
       <Tab.Screen name="Movies" component={Movies} />
       <Tab.Screen name="Tv" component={Tv} />
       <Tab.Screen name="Search" component={Search} />
