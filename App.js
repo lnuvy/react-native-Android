ccdf5fbea324eadd97fa68b07db9e9ef8ab1039a
lnuvy <hangeulgod@naver.com>
2022-01-25 15:32:12 +0900

    useFonts, useAssets (로딩과정에서 다른 작업이 필요하면  사용하기 좋지않음)

--- App.js
+++ App.js
@@ -2,42 +2,17 @@ import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
 import { Text, Image } from "react-native";
 import * as Font from "expo-font";
+import { useFonts } from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-const loadImages = (images) =>
-  images.map((image) => {
-    // url을 인자로 갖고있으면 (서버 등의 외부에서 가져올때)
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-      // 프로젝트 내부에 있으면
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require("./picture.jpg"),
-      "https://reactnative.dev/img/oss_logo.png",
-    ]);
-    //                 두개의 배열을 합치기
-    await Promise.all(...fonts, ...images);
-  };
+  // 코드가 간결해지지만 Image.prefetch() 를 사용할 수 없음
+  const [assets] = useAssets([require("./picture.jpg")]);
+  const [loaded] = useFonts(Ionicons.font);
 
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.log}
-      />
-    );
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done Loading!</Text>;
 }
