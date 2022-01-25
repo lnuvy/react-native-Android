0cf84e4ef27a04119a05f544f37b69a592c2e212
lnuvy <hangeulgod@naver.com>
2022-01-25 18:58:34 +0900

    isDark 를 쉽게만들어주는 theme

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Text, Image } from "react-native";
+import { Text, Image, useColorScheme } from "react-native";
 import * as Font from "expo-font";
 import { useFonts } from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
@@ -9,19 +9,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import Stack from "./navigation/Stack";
 import Root from "./navigation/Root";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./styled";
 
 export default function App() {
-  // 코드가 간결해지지만 Image.prefetch() 를 사용할 수 없음
   const [assets] = useAssets([require("./picture.jpg")]);
   const [loaded] = useFonts(Ionicons.font);
+
+  const isDark = useColorScheme() === "dark";
 
   if (!assets || !loaded) {
     return <AppLoading />;
   }
   return (
     // 사용하려면 반드시 임포트 필요
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
---
+++ styled.js
@@ -0,0 +1,8 @@
+export const lightTheme = {
+  mainBgColor: "white",
+  textColor: "#808e9b", // GRAY_COLOR
+};
+export const darkTheme = {
+  mainBgColor: "#808e9b", // GRAY_COLOR
+  textColor: "#485460", // dark gray
+};
