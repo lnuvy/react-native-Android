3ac4746df71dfac6538bfa8ff533f8dc2d2dbc8e
lnuvy <hangeulgod@naver.com>
2022-01-25 15:13:24 +0900

    현재 없어진방법

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
 import { Text } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
+  const [assets, error] = useAssets([
+    require("./picture.jpg"),
+    require("./down2.jpg"),
+  ]);
+
   const startLoading = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await Font.loadAsync(Ionicons.font);
+    await await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
   };
   if (!ready) {
     return (
