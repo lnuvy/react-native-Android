f74004d380625dc7e4167e0c32ef73ec93f1ab41
lnuvy <hangeulgod@naver.com>
2022-01-25 18:46:52 +0900

    props 를 전해줘서 CSS 꾸미기도 가능하다

--- screens/Movies.js
+++ screens/Movies.js
@@ -3,31 +3,23 @@ import { View, Text, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
-const Btn = styled.View`
+const Btn = styled.TouchableOpacity`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: red;
 `;
 
+const Title = styled.Text`
+  color: ${(props) => (props.selected ? "blue" : "red")};
+`;
+
+const Header = styled.View``;
+
 const Movies = ({ navigation: { navigate } }) => (
-  <Btn
-    onPress={() => navigate("Stack", { screen: "Three" })}
-    style={styles.btn}
-  >
-    <Text>Movies</Text>
+  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+    <Title selected={true}>Movies</Title>
+    <Title selected={false}>Movies</Title>
   </Btn>
 );
-
-const styles = StyleSheet.create({
-  btn: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    color: "blue",
-  },
-});
 
 export default Movies;
