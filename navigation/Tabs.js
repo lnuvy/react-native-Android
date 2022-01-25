e2a6cd54eedb6dfd06f0e2e2c9db5a6bc2599272
lnuvy <hangeulgod@naver.com>
2022-01-25 16:12:09 +0900

    https://reactnavigation.org/docs/bottom-tab-navigator/ 프롭스 사용해보기

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,13 +3,31 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { Text, View } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
+  <Tab.Navigator
+    // screenOptions = 모든 레이블에 적용 , options = 각각의 컴포넌트 적용
+    screenOptions={{
+      tabBarStyle: { backgroundColor: "tomato" },
+      tabBarLabelPosition: "beside-icon",
+    }}
+  >
+    <Tab.Screen
+      name="Movies"
+      component={Movies}
+      options={{
+        headerTitleStyle: { color: "cyan" },
+        headerRight: () => (
+          <View>
+            <Text>Hello</Text>
+          </View>
+        ),
+      }}
+    />
+    <Tab.Screen name="Tv" component={Tv} options={{ tabBarBadge: 5 }} />
     <Tab.Screen name="Search" component={Search} />
   </Tab.Navigator>
 );
