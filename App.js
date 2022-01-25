c57e9f33c40fc7968b69a80ce1177872043e91ac
lnuvy <hangeulgod@naver.com>
2022-01-25 15:27:16 +0900

    유틸리티 함수 작성

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) =>
+  images.map((image) => {
+    // url을 인자로 갖고있으면 (서버 등의 외부에서 가져올때)
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+      // 프로젝트 내부에 있으면
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
-  const [assets, error] = useAssets([
-    require("./picture.jpg"),
-    require("./down2.jpg"),
-  ]);
-
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./picture.jpg"),
+      "https://reactnative.dev/img/oss_logo.png",
+    ]);
+    //                 두개의 배열을 합치기
+    await Promise.all(...fonts, ...images);
   };
+
   if (!ready) {
     return (
       <AppLoading
