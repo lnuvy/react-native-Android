aa5b29ffeeb35072cb27e5caefca497503c1ae4a
lnuvy <hangeulgod@naver.com>
2022-01-25 16:56:09 +0900

    tabBarIcon 함수로 focused, color 인자 넘겨주기

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Text, Image, useColorScheme } from "react-native";
+import { Text, Image } from "react-native";
 import * as Font from "expo-font";
 import { useFonts } from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
-import {
-  NavigationContainer,
-  DarkTheme,
-  DefaultTheme,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 
 export default function App() {
@@ -17,14 +13,12 @@ export default function App() {
   const [assets] = useAssets([require("./picture.jpg")]);
   const [loaded] = useFonts(Ionicons.font);
 
-  const isDark = useColorScheme() === "dark";
-
   if (!assets || !loaded) {
     return <AppLoading />;
   }
   return (
     // 사용하려면 반드시 임포트 필요
-    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+    <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   );
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -6,16 +6,73 @@ import Search from "../screens/Search";
 import { Text, View } from "react-native";
 import { useColorScheme } from "react-native";
 import { BLACK_COLOR, GRAY_COLOR, YELLOW_COLOR } from "../colors";
+import { Ionicons } from "@expo/vector-icons";
+import Profile from "../screens/Profile";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-  const isDark = useColorScheme() === "dark";
+  const isDark = useColorScheme() !== "dark";
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="Tv" component={Tv} />
-      <Tab.Screen name="Search" component={Search} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        tabBarInactiveTintColor: GRAY_COLOR,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        },
+        tabBarLabelStyle: {
+          marginTop: -5,
+          fontSize: 12,
+          fontWeight: "900",
+          marginBottom: 5,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon: ({ focused, color }) => {
+            return <Ionicons name="film-outline" color={color} size={22} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ focused, color }) => {
+            return <Ionicons name="tv-outline" color={color} size={22} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color }) => {
+            return <Ionicons name="search-outline" color={color} size={22} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused, color }) => {
+            return (
+              <Ionicons name="person-circle-outline" color={color} size={22} />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
