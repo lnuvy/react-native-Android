ccdb08e5d6d09129100fc7654d3d3e54f6620ca9
lnuvy <hangeulgod@naver.com>
2022-01-25 20:09:20 +0900

    typescript 파일 prettier 적용시킴

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,31 +1,53 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
-import { Dimensions } from "react-native";
+import React, { useState } from "react";
+import { ActivityIndicator, Dimensions } from "react-native";
 import styled from "styled-components/native";
-import Swiper from 'react-native-web-swiper';
+import Swiper from "react-native-web-swiper";
 
-const Container = styled.ScrollView`
-  background-color: ${props => props.theme.mainBgColor};
-`
+const API_KEY = "71f43de951c136a669bee61f1fbf4c5b";
+
+const Container = styled.ScrollView``;
 
 const View = styled.View`
   flex: 1;
-`
+`;
+
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
-const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
-  return (
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const [loading, setLoading] = useState(true);
+
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    );
+  };
+  return loading ? (
+    <Loader>
+      <ActivityIndicator size="large" />
+    </Loader>
+  ) : (
     <Container>
-      <Swiper containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}>
-        <View style={{ backgroundColor: "red"}}></View>
-        <View style={{ backgroundColor: "blue"}}></View>
-        <View style={{ backgroundColor: "cyan"}}></View>
-        <View style={{ backgroundColor: "tomato"}}></View>
-        <View style={{ backgroundColor: "black"}}></View>
+      <Swiper
+        controlsEnabled={false}
+        loop
+        timeout={3}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "cyan" }}></View>
+        <View style={{ backgroundColor: "tomato" }}></View>
+        <View style={{ backgroundColor: "black" }}></View>
       </Swiper>
     </Container>
-  )
-}
+  );
+};
 
 export default Movies;
