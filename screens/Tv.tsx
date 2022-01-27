7a6ac4b226376cae126be16c12d288fdac297f75
lnuvy <hangeulgod@naver.com>
2022-01-27 17:04:29 +0900

    IOS 유저 TV 스크린 새로고침 튀어오르는모션 개선 / Movies 는 고치지않음(비교)

--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { View, Text, ScrollView, FlatList, RefreshControl } from "react-native";
 import { QueryClient, useQuery, useQueryClient } from "react-query";
 import { tvAPI } from "../api";
@@ -7,31 +8,28 @@ import Loader from "../components/Loader";
 import VMedia from "../components/VMedia";
 
 const Tv = () => {
+  const [refreshing, setRefreshing] = useState(false);
   const queryClient = useQueryClient();
-  const {
-    isLoading: todayLoading,
-    data: todayData,
-    isRefetching: todayRefetching,
-  } = useQuery(["tv", "today"], tvAPI.airingToday);
-  const {
-    isLoading: topLoading,
-    data: topData,
-    isRefetching: topRefetching,
-  } = useQuery(["tv", "top"], tvAPI.topRated);
-  const {
-    isLoading: trendingLoading,
-    data: trendingData,
-    isRefetching: trendingRefetching,
-  } = useQuery(["tv", "trending"], tvAPI.trending);
+  const { isLoading: todayLoading, data: todayData } = useQuery(
+    ["tv", "today"],
+    tvAPI.airingToday
+  );
+  const { isLoading: topLoading, data: topData } = useQuery(
+    ["tv", "top"],
+    tvAPI.topRated
+  );
+  const { isLoading: trendingLoading, data: trendingData } = useQuery(
+    ["tv", "trending"],
+    tvAPI.trending
+  );
 
   const loading = todayLoading || topLoading || trendingLoading;
 
-  const onRefresh = () => {
-    queryClient.refetchQueries(["tv"]);
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await queryClient.refetchQueries(["tv"]);
+    setRefreshing(false);
   };
-  const refreshing = todayRefetching || topRefetching || trendingRefetching;
-
-  console.log(refreshing);
 
   if (loading) {
     return <Loader />;
